5f0bdb1458e1602b3d4ec855d4adb22bde4aa3a0
Savannah Smith <ssmith1695@ivytech.edu>
2023-05-10 17:49:07 -0500

    Creation of KnittedUserManual.py. Updated list to be alphabetized. Documentation and explanation of variables added.

---
+++ myapp/KnittedUserManual.py
@@ -0,0 +1,37 @@
+'''
+
+Knitted: Yarn Collection User Manual
+
+Overview
+Knitted is a simple GUI application that helps yarn craft lovers keep track of their yarn collection. With Knitted,
+you can add new yarns to your collection, view a list of your current yarns, and select and view details about
+individual yarns.
+
+Getting started
+To run Knitted, you will need to have Python 3 installed on your computer. Once you have Python 3 installed, you can
+run Knitted by running the knitted.py file in your terminal or IDE.
+
+Adding yarn
+To add a new yarn to your collection, follow these steps:
+
+Enter the color of the yarn in the "Color" field.
+Enter the weight of the yarn in the "Weight" field. The weight scale ranges from 1 (superfine) to 7 (jumbo).
+Enter the material of the yarn in the "Material" field.
+Click the "Add Yarn" button to add the new yarn to your collection.
+Viewing your yarn collection
+To view your current yarn collection, look at the three list boxes in the middle of the Knitted window.
+The leftmost list box displays the colors of your yarns, the middle list box displays the weights of your yarns,
+and the rightmost list box displays the materials of your yarns.
+
+About Knitted
+To learn more about Knitted, click the "About" button to see information about the purpose of the app and its creator.
+
+Exiting Knitted
+To exit Knitted, click the "Exit" button or close the Knitted window.
+
+Troubleshooting
+If you experience any issues while using Knitted, please reach out to the app's creator for support.
+
+Conclusion
+I hope you enjoy using Knitted to organize and keep track of your yarn collection!
+'''
