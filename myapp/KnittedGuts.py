d71aba689cc2c9cf1244587aea3f7a3b61d8eb99
Savannah Smith <ssmith1695@ivytech.edu>
2023-05-10 17:49:07 -0500

    Creation of KnittedUserManual.py. Updated list to be alphabetized.

--- myapp/KnittedGuts.py
+++ myapp/KnittedGuts.py
@@ -14,12 +14,22 @@ def addToIndex(key, index, yarn):
 
 class YarnCollection:
     def __init__(self):
+        """
+        A collection of yarns, each represented as a tuple of (color, weight, material)
+        """
         self.collection = {}
         self.colors = {}
         self.weights = {}
         self.materials = {}
 
     def addYarn(self, color, weight, material):
+        """
+        Add a new yarn to the collection.
+        Args:
+            color (str): The color of the yarn.
+            weight (str): The weight of the yarn, from 1 (superfine) to 7 (jumbo).
+            material (str): The material of the yarn.
+        """
         yarn = (color, weight, material)
         self.collection.setdefault(yarn, 0)
         self.collection[yarn] += 1
@@ -37,13 +47,30 @@ class YarnCollection:
         return self.materials.get(material, [])
 
 
+def syncListBoxes(source, dest1, dest2):
+    # Synchronize the selected item in source with dest1 and dest2
+    selected = source.curselection()
+    if selected:
+        index = selected[0]
+        dest1.selection_clear(0, tk.END)
+        dest1.selection_set(index)
+        dest2.selection_clear(0, tk.END)
+        dest2.selection_set(index)
+
+
 class YarnCollectionApp:
     def __init__(self, master):
+        """
+        Initialize the GUI.
+        Args:
+            master: The Tkinter root object.
+        """
         self.master = master
         master.title("Knitted: Yarn Collection")
 
         self.collection = YarnCollection()
 
+        # Create labels and entry boxes for the color, weight, and material of a yarn
         self.colorLabel = tk.Label(master, text="Color:")
         self.colorLabel.grid(row=0, column=0)
 
@@ -62,9 +89,11 @@ class YarnCollectionApp:
         self.materialEntry = tk.Entry(master)
         self.materialEntry.grid(row=2, column=1)
 
+        # Create a button for adding a new yarn to the collection
         self.addButton = tk.Button(master, text="Add Yarn", command=self.addYarn)
         self.addButton.grid(row=3, column=0)
 
+        # Create list boxes for displaying the collection of yarns
         self.colorListbox = tk.Listbox(master)
         self.colorListbox.grid(row=4, column=0)
 
@@ -74,6 +103,7 @@ class YarnCollectionApp:
         self.materialListbox = tk.Listbox(master)
         self.materialListbox.grid(row=4, column=2)
 
+        # Create buttons for showing help and about windows, and for exiting the program
         self.helpButton = tk.Button(master, text="Help", command=self.showHelpWindow)
         self.helpButton.grid(row=5, column=0)
 
@@ -84,14 +114,21 @@ class YarnCollectionApp:
         self.exitButton.grid(row=5, column=2)
 
     def updateListbox(self):
+        """
+        Update the list boxes with the current collection of yarns.
+        """
         self.colorListbox.delete(0, tk.END)
         self.weightListbox.delete(0, tk.END)
         self.materialListbox.delete(0, tk.END)
 
+        # Convert each yarn tuple to a dictionary for easy access to the color, weight, and material
         yarns = []
         for yarn in self.collection.collection:
             color, weight, material = yarn
             yarns.append({"color": color, "weight": weight, "material": material})
+
+        # sort the yarns by color, weight, and material
+        yarns.sort(key=lambda yarn: (yarn["color"], yarn["weight"], yarn["material"]))
 
         for yarn in yarns:
             self.colorListbox.insert(tk.END, yarn["color"])
@@ -99,25 +136,12 @@ class YarnCollectionApp:
             self.materialListbox.insert(tk.END, yarn["material"])
 
         # Bind the list boxes together, so they highlight the same item
-        self.colorListbox.bind('<<ListboxSelect>>', lambda event: self.syncListBoxes(event, self.colorListbox,
-                                                                                     self.weightListbox,
-                                                                                     self.materialListbox))
-        self.weightListbox.bind('<<ListboxSelect>>', lambda event: self.syncListBoxes(event, self.weightListbox,
-                                                                                      self.colorListbox,
-                                                                                      self.materialListbox))
-        self.materialListbox.bind('<<ListboxSelect>>', lambda event: self.syncListBoxes(event, self.materialListbox,
-                                                                                        self.colorListbox,
-                                                                                        self.weightListbox))
-
-    def syncListBoxes(self, source, dest1, dest2):
-        # Synchronize the selected item in source with dest1 and dest2
-        selected = source.curselection()
-        if selected:
-            index = selected[0]
-            dest1.selection_clear(0, tk.END)
-            dest1.selection_set(index)
-            dest2.selection_clear(0, tk.END)
-            dest2.selection_set(index)
+        self.colorListbox.bind('<<ListboxSelect>>', lambda event: syncListBoxes(event, self.colorListbox,
+                                                                                self.weightListbox))
+        self.weightListbox.bind('<<ListboxSelect>>', lambda event: syncListBoxes(event, self.weightListbox,
+                                                                                 self.colorListbox))
+        self.materialListbox.bind('<<ListboxSelect>>', lambda event: syncListBoxes(event, self.materialListbox,
+                                                                                   self.colorListbox))
 
     def addYarn(self):
         color = self.colorEntry.get()
